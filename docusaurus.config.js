1e171d18b8ac368d8f995060095a1e00ce91f551
braginini <bangvalo@gmail.com>
2021-11-20 15:44:36 +0100

    add logos

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Wiretrustee Docs',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
@@ -43,9 +43,9 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Wiretrustee Docs',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Wiretrustee Logo',
           src: 'img/logo.svg',
         },
         items: [
